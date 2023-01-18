54448b657ce2399384692b03bdf6813fc68d284f
richelbilderbeek <richel@richelbilderbeek.nl>
2023-01-18 16:22:32 +0100

    Around 118 percent chance to find something significant for 2 things

--- simmons_et_al_2011.R
+++ simmons_et_al_2011.R
@@ -121,15 +121,85 @@ ggplot2::ggsave(paste0("distribution_", n_subjects, "_", best_seed, ".png"), wid
 
 #' Determines if 'red' is significantly different than 'green', for a covariate
 create_experiment_results_with_covariate <- function(n_subjects, n_seeds) {
-  t_result <- tibble::tibble(seed = seq_len(n_seeds), p_value = NA)
+  t_result <- tibble::tibble(
+    seed = seq_len(n_seeds), 
+    p_value_preference = NA,
+    p_value_preference_for_odd = NA,
+    p_value_preference_for_even = NA,
+    p_value_phone_number = NA,
+    p_value_phone_number_for_red = NA,
+    p_value_phone_number_for_green = NA
+  )
   for (i in seq_len(n_seeds)) {
     seed <- t_result$seed[i]
     t <- create_subjects(n_subjects, seed = seed)
-    p_value <- t.test(
+    p_value_preference <- t.test(
       x = t[t$preference == "red", ]$length, 
       y = t[t$preference == "green", ]$length
     )$p.value
-    t_result$p_value[i] <- p_value
+    p_value_preference_for_odd <- t.test(
+      x = t[t$preference == "red" & t$phone_number == "odd", ]$length, 
+      y = t[t$preference == "green" & t$phone_number == "odd", ]$length
+    )$p.value
+    p_value_preference_for_even <- t.test(
+      x = t[t$preference == "red" & t$phone_number == "even", ]$length, 
+      y = t[t$preference == "green" & t$phone_number == "even", ]$length
+    )$p.value
+    p_value_phone_number <- t.test(
+      x = t[t$phone_number == "odd", ]$length, 
+      y = t[t$phone_number == "even", ]$length
+    )$p.value
+    p_value_phone_number_for_red <- t.test(
+      x = t[t$phone_number == "odd" & t$preference == "red", ]$length, 
+      y = t[t$phone_number == "even" & t$preference == "red", ]$length
+    )$p.value
+    p_value_phone_number_for_green <- t.test(
+      x = t[t$phone_number == "odd" & t$preference == "green", ]$length, 
+      y = t[t$phone_number == "even" & t$preference == "green", ]$length
+    )$p.value
+    t_result$p_value_preference[i] <- p_value_preference
+    t_result$p_value_preference_for_odd[i] <- p_value_preference_for_odd
+    t_result$p_value_preference_for_even[i] <- p_value_preference_for_even
+    t_result$p_value_phone_number[i] <- p_value_phone_number
+    t_result$p_value_phone_number_for_red[i] <- p_value_phone_number_for_red
+    t_result$p_value_phone_number_for_green[i] <- p_value_phone_number_for_green
   }
   t_result
 }
+
+
+n_subjects <- 1000
+n_seeds <- 1000
+t <- create_experiment_results_with_covariate(n_subjects = n_subjects, n_seeds = n_seeds)
+
+t$p_value_max <- NA
+for (i in seq_len(nrow(t))) {
+  t$p_value_max[i] <- min(as.numeric(t[i, 2:7]))
+}
+sum(t$p_value_max < 0.05)
+n_significant <- sum(t$p_value_preference < 0.05 | 
+  t$p_value_preference_for_odd < 0.05 | 
+  t$p_value_preference_for_even < 0.05 |  
+  t$p_value_phone_number < 0.05 | 
+  t$p_value_phone_number_for_red < 0.05 | 
+  t$p_value_phone_number_for_green < 0.05)
+percentage_significant <- 100.0 * n_significant / n_seeds
+percentage_significant
+
+ggplot2::ggplot(
+  t, 
+  ggplot2::aes(x = p_value_max)
+) + 
+  ggplot2::geom_histogram(binwidth = 0.01) + 
+  ggplot2::scale_x_continuous("Lowest p-value", limits = c(0, 1), minor_breaks = seq(0.0, 1.0, by = 0.01)) + 
+  ggplot2::geom_vline(xintercept = 0.05, lty = "dashed") +
+  ggplot2::labs(
+    caption = paste0(
+      "Number of experiments: ", n_seeds, "\n",
+      "Number of subjects in an experiment: ", n_subjects, "\n",
+      "Number of things: ", 2, "\n",
+      "Significant findings: ", percentage_significant, "%"
+    )
+  ) +
+  ggplot2::theme(text = ggplot2::element_text(size = 20))
+ggplot2::ggsave(paste0("n_significant_findings_", n_subjects, "_", n_seeds, "_2_things.png"), width = 7, height = 7)
